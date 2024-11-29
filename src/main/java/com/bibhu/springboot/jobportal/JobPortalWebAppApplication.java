bc2ed8aca08a2dbfc8ba14ec217a930b8e81de6e
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2024-11-29 22:05:20 +0530

    initial commit

---
+++ src/main/java/com/bibhu/springboot/jobportal/JobPortalWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bibhu.springboot.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalWebAppApplication.class, args);
+	}
+
+}
